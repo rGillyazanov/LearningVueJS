2caae675edd3f5685c4d3a2d225b1460754bdeab
rGillyazanov <rgillyazanov@yandex.ru>
2020-05-24 16:52:58 +0300

    Урок 1. Создание своей директивы.

--- Vue CLIWebpack/vue-project/src/App.vue
+++ Vue CLIWebpack/vue-project/src/App.vue
@@ -1,29 +1,18 @@
 <template>
     <div>
-        <car>
-            <h2 slot="title">{{ carName }}</h2>
-            <p slot="text">Lorem ipsum dolor.</p>
-        </car>
+        <h2>{{ title }}</h2>
     </div>
 </template>
 
 <script>
-import Car from './Car'
-
 export default {
     data() {
         return {
-            carName: 'Ford'
+            title: 'Hello I am div'
         }
-    },
-    components: {
-        car: Car
     }
 }
 </script>
 
 <style scoped>
-    h2 {
-        color: red;
-    }
 </style>
--- Vue CLIWebpack/vue-project/src/main.js
+++ Vue CLIWebpack/vue-project/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ColorDirective from "./color";
 
-export const eventEmitter = new Vue();
+Vue.directive('colored', ColorDirective);
 
 new Vue({
   el: '#app',
