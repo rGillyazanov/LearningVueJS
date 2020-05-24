c906e21fed070e96a095d871a56143a5ae6ff1fa
rGillyazanov <rgillyazanov@yandex.ru>
2020-05-24 21:49:30 +0300

    Урок 7. Локальная регистрация.

--- Vue CLIWebpack/vue-project/src/App.vue
+++ Vue CLIWebpack/vue-project/src/App.vue
@@ -2,6 +2,8 @@
     <div>
         <h2 v-colored:background.font="'red'">{{ title }}</h2>
         <h2 v-colored:color.delay.font="'yellow'">{{ title }}</h2>
+
+        <h2 v-font>Local font directive</h2>
     </div>
 </template>
 
@@ -10,6 +12,13 @@ export default {
     data() {
         return {
             title: 'Hello I am div'
+        }
+    },
+    directives: {
+        font: {
+            bind(el, bindings, vNode) {
+                el.style.fontSize = '40px'
+            }
         }
     }
 }
