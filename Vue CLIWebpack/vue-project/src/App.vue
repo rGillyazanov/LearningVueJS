530826e3420d760645d5c0f80e49878256667e5d
rGillyazanov <rgillyazanov@yandex.ru>
2020-06-02 09:04:41 +0300

    Урок 3. Создание навигации.

--- Vue CLIWebpack/vue-project/src/App.vue
+++ Vue CLIWebpack/vue-project/src/App.vue
@@ -118,6 +118,18 @@
                     </form>
                 </div>
                 <div class="col-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <router-link class="nav-link" to="/">Home</router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link class="nav-link" to="/cars">Cars</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                     <router-view></router-view>
                 </div>
             </div>
