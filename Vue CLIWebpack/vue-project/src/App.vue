08641436742be599d94b56efc196845ed62127a1
rGillyazanov <rgillyazanov@yandex.ru>
2020-06-02 09:09:51 +0300

    Урок 4. Обозначение активной ссылки.

--- Vue CLIWebpack/vue-project/src/App.vue
+++ Vue CLIWebpack/vue-project/src/App.vue
@@ -121,12 +121,12 @@
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                         <div class="collapse navbar-collapse">
                             <ul class="navbar-nav">
-                                <li class="nav-item active">
-                                    <router-link class="nav-link" to="/">Home</router-link>
-                                </li>
-                                <li class="nav-item">
-                                    <router-link class="nav-link" to="/cars">Cars</router-link>
-                                </li>
+                                <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+                                    <a class="nav-link">Home</a>
+                                </router-link>
+                                <router-link tag="li" class="nav-item" exact to="/cars" active-class="active">
+                                    <a class="nav-link">Cars</a>
+                                </router-link>
                             </ul>
                         </div>
                     </nav>
