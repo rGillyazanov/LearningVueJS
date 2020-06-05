121e612b658b101ef9e5f15c28d7d67b798073d7
rGillyazanov <rgillyazanov@yandex.ru>
2020-06-05 22:41:39 +0300

    Урок 12. Ленивая загрузка.

--- Vue CLIWebpack/vue-project/src/routes.js
+++ Vue CLIWebpack/vue-project/src/routes.js
@@ -1,9 +1,15 @@
 import VueRouter from "vue-router";
 import Home from "./pages/Home";
-import Cars from "./pages/Cars";
 import Car from "./pages/Car";
 import CarFull from "./pages/CarFull";
 import ErrorCmp from "./pages/Error";
+
+// Lazy loading
+const Cars = resolve => {
+    require.ensure(['./pages/Cars.vue'], () => {
+        resolve(require('./pages/Cars'))
+    });
+}
 
 export default new VueRouter({
     routes: [
